771ace20ae610159d40185f4fcf162d92509a0be
yaStigma <yastigma@gmail.com>
2023-11-22 18:48:55 +0000

    Deploying to gh-pages from @ yaStigma/goit-js-hw-10@2d7b1da649581730fa24d9e80b02f88ed00e0457 🚀

---
+++ index.48623566.js
@@ -0,0 +1,2 @@
+document.querySelector(".cat-info"),document.querySelector(".breed-select");
+//# sourceMappingURL=index.48623566.js.map
---
+++ index.b1184fa8.js
@@ -0,0 +1,2 @@
+document.querySelector(".cat-info"),document.querySelector(".breed-select");
+//# sourceMappingURL=index.b1184fa8.js.map
